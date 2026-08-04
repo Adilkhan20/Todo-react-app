1fb068ee0e7568dfed8def270db8a2978c59c9b3
adilkhan20 <adilkhansumbal7@gmail.com>
2026-08-04 18:22:18 +0500

    add updated heading

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -3,7 +3,9 @@ import style from "./Welcome.module.css"
 
 const Welcome = () => {
   return (
-  <h2>enjoy your day react</h2>)
+  <h2>enjoy your day react
+    hi i am adil as developer in trot tk 
+  </h2>)
 };
 
 export default Welcome;
