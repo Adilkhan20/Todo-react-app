d92a165170d4b9ee78ce510d4c77b0db9ef170ee
HussainHanif <codewithdevhussain@gmail.com>
2026-08-04 16:29:39 +0500

    changing the color of the button

--- src/Components/Todoitem.jsx
+++ src/Components/Todoitem.jsx
@@ -8,7 +8,7 @@ const Todoitem = ({item,date,handleDelete}) => {
         <div className="col col-lg-2">{item}</div>
         <div className="col-3">{date}</div>
         <div className="col-auto">
-          <button type="button" className="btn btn-danger" onClick={()=>handleDelete(item)}>
+          <button type="button" className="btn btn-success" onClick={()=>handleDelete(item)}>
             Delete
           </button>
         </div>
