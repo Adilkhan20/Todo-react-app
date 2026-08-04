56e6b99a4a234d2e0e7c58200e6fcc9b7e7aaa7e
adilkhan20 <adilkhansumbal7@gmail.com>
2026-08-04 19:28:23 +0500

    editing welcome paragraph

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -3,7 +3,7 @@ import style from "./Welcome.module.css"
 
 const Welcome = () => {
   return (
-  <h2>enjoy your day react</h2>)
+  <h2>enjoy your day react hi i am making changings in welcome folder</h2>)
 };
 
 export default Welcome;
